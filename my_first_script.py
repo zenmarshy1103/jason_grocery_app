d8d6729d0f7fd48a29d6d5d003c2c9ad14cea695
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2024-12-23 22:46:06 +1300

    Add files via upload
    Added  Day 2 Exercises:
        - Exercise 1: Calculation
        - Exercise 2 : String Manipulation
        - Exercise 3: Common Errors

---
+++ my_first_script.py
@@ -0,0 +1,22 @@
+# DAY 2
+# Exercise 1: Calculation
+# milk = 2.5
+# bread = 3.80
+# eggs = 2
+
+# cost = milk + bread + float(eggs)
+# print(f"Your totla is: ${cost}.")
+# print("\n")
+
+# Exercise 2: String Manipulation
+# opening_phrase = "Welcome to "    # Setting a opening phrase 
+# user = input("What is the store name: ") # get the name from the user via input command
+
+# print(opening_phrase + user + "!.") # Concatenate opening phrase and the input entered by the user
+# print("\n") # Add a new line to the screen               
+
+# Exercise 3: Common Errors
+milk = "3"
+bread = 2.5
+total = float(milk) + bread
+print(f"The total cost is: ${total}")
